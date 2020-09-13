64b7e0b7c861d6aa2211074046ab4f75ae603ab1
TakbeerAli <Takbeeralikhan@gmail.com>
2020-09-13 21:14:52 +0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
